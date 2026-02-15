b30671519fc345af0fb8a561d6b138d12c61a5a8
Flashtate98 <106729837+flashtate98@users.noreply.github.com>
2026-02-15 17:45:25 -0600

    Update PlayerbotAI.cpp

--- src/Bot/PlayerbotAI.cpp
+++ src/Bot/PlayerbotAI.cpp
@@ -1558,6 +1558,9 @@ void PlayerbotAI::ApplyInstanceStrategies(uint32 mapId, bool tellMaster)
         case 548:
             strategyName = "ssc";  // Serpentshrine Cavern
             break;
+        case 558:
+            strategyName = "tbc-ac"; // Auchindoun: Auchenai Crypts
+            break;
         case 565:
             strategyName = "gruulslair";  // Gruul's Lair
             break;
