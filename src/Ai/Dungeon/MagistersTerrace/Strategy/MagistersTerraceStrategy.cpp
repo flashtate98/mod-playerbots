d2a4f4ae36483173c52088e7cfbfc97293a01ee8
Flashtate98 <106729837+flashtate98@users.noreply.github.com>
2026-04-18 13:04:04 -0500

    Even more dumb fixes

--- src/Ai/Dungeon/MagistersTerrace/Strategy/MagistersTerraceStrategy.cpp
+++ src/Ai/Dungeon/MagistersTerrace/Strategy/MagistersTerraceStrategy.cpp
@@ -8,3 +8,8 @@ void TbcDungeonMagistersTerraceStrategy::InitTriggers(std::vector<TriggerNode*>&
     triggers.push_back(new TriggerNode("magic dampening field", {
         NextAction("avoid magic dampening field", ACTION_EMERGENCY + 10) }));
 }
+
+void TbcDungeonMagistersTerraceStrategy::InitMultipliers(std::vector<Multiplier*>& multipliers)
+{
+    multipliers.push_back(new AvoidMagicDampeningFieldMultiplier(botAI));
+}
