9ea71f9c235ac205dce726571fef61a88f7c49cd
Flashtate98 <106729837+flashtate98@users.noreply.github.com>
2026-04-22 20:30:04 -0500

    Bubble Avoidance Overhaul

--- src/Ai/Dungeon/MagistersTerrace/Action/MagistersTerraceActions.cpp
+++ src/Ai/Dungeon/MagistersTerrace/Action/MagistersTerraceActions.cpp
@@ -12,41 +12,17 @@ bool AvoidMagicDampeningFieldAction::Execute(Event /*event*/)
     if (!aura)
         return false;
 
-    DynamicObject* dynObj = aura->GetDynobjOwner();
-    if (!dynObj)
+    DynamicObject* bubble = aura->GetDynobjOwner();
+    if (!bubble)
         return false;
 
-    float radius = dynObj->GetRadius();
-    const SpellInfo* sInfo = sSpellMgr->GetSpellInfo(dynObj->GetSpellId());
-    if (radius <= 0.0f && sInfo)
+    constexpr float safeDistFromBubble = 7.0f;
+
+    if (bot->GetExactDist2d(bubble) < safeDistFromBubble)
     {
-        for (int e = 0; e < MAX_SPELL_EFFECTS; ++e)
-        {
-            auto const& eff = sInfo->Effects[e];
-            if (eff.Effect == SPELL_EFFECT_SCHOOL_DAMAGE ||
-                eff.Effect == SPELL_EFFECT_APPLY_AURA)
-            {
-                radius = eff.CalcRadius();
-                break;
-            }
-        }
+        botAI->Reset();
+        return FleePosition (bubble->GetPosition(), safeDistFromBubble);
     }
 
-    if (radius <= 0.0f)
-        return false;
-
-    constexpr float bufferDist = 2.0f;
-    float distToObj = bot->GetExactDist2d(dynObj->GetPositionX(), dynObj->GetPositionY());
-
-    float dx = bot->GetPositionX() - dynObj->GetPositionX();
-    float dy = bot->GetPositionY() - dynObj->GetPositionY();
-    float safeDist = radius + bufferDist;
-
-    float invDist = 1.0f / distToObj;
-    float moveX = dynObj->GetPositionX() + (dx * invDist) * safeDist;
-    float moveY = dynObj->GetPositionY() + (dy * invDist) * safeDist;
-
-    botAI->Reset();
-    return MoveTo(bot->GetMapId(), moveX, moveY, bot->GetPositionZ(), false, false, false,
-                  true, MovementPriority::MOVEMENT_FORCED, true, false);
+    return false;
 }
