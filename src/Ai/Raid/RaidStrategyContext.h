25e3d942f356ae5db9de0c525993e9ae03b47147
Flashtate98 <106729837+flashtate98@users.noreply.github.com>
2026-07-15 19:21:50 -0500

    Merge branch 'the-lab' of https://github.com/brighton-chi/mod-playerbots.git into Testing

--- src/Ai/Raid/RaidStrategyContext.h
+++ src/Ai/Raid/RaidStrategyContext.h
@@ -35,7 +35,6 @@ public:
         creators["magtheridon"] = &RaidStrategyContext::magtheridon;
         creators["naxx"] = &RaidStrategyContext::naxx;
         creators["ssc"] = &RaidStrategyContext::ssc;
-        creators["zulaman"] = &RaidStrategyContext::zulaman;
         creators["tempestkeep"] = &RaidStrategyContext::tempestkeep;
         creators["hyjal"] = &RaidStrategyContext::hyjal;
         creators["blacktemple"] = &RaidStrategyContext::blacktemple;
@@ -58,7 +57,6 @@ private:
     static Strategy* magtheridon(PlayerbotAI* botAI) { return new RaidMagtheridonStrategy(botAI); }
     static Strategy* naxx(PlayerbotAI* botAI) { return new RaidNaxxStrategy(botAI); }
     static Strategy* ssc(PlayerbotAI* botAI) { return new RaidSSCStrategy(botAI); }
-    static Strategy* zulaman(PlayerbotAI* botAI) { return new RaidZulAmanStrategy(botAI); }
     static Strategy* tempestkeep(PlayerbotAI* botAI) { return new RaidTempestKeepStrategy(botAI); }
     static Strategy* hyjal(PlayerbotAI* botAI) { return new RaidHyjalSummitStrategy(botAI); }
     static Strategy* blacktemple(PlayerbotAI* botAI) { return new RaidBlackTempleStrategy(botAI); }
